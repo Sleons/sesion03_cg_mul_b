71d3f2117b5a4bc763ffac03aef52f1c273d6d76
sleons <est.santiagoa.leon@unimilitar.edu.co>
2021-08-12 16:12:44 -0500

    Subido por: sleons <est.santiagoa.leon@unimilitar.edu.co>
    Subido por: sleons <est.santiagoa.leon@unimilitar.edu.co>
    
    Subido por: sleons <est.santiagoa.leon@unimilitar.edu.co>

---
+++ SistemasCoordenadas2D/src/sistemascoordenadas2d/Cilindrica.java
@@ -0,0 +1,36 @@
+package sistemascoordenadas2d;
+
+public class Cilindrica {
+
+	private double radio;
+	private double angulo;
+	private double z;
+	
+	
+	public double getRadio() {
+		return radio;
+	}
+	public void setRadio(double radio) {
+		this.radio = radio;
+	}
+	public double getAngulo() {
+		return angulo;
+	}
+	public void setAngulo(double angulo) {
+		this.angulo = angulo;
+	}
+	public double getZ() {
+		return z;
+	}
+	public void setZ(double z) {
+		this.z = z;
+	}
+	
+	public Cilindrica cilindrica_cartesiana3D(double r , double angulo) {
+		
+		Cilindrica obj = new Cilindrica();
+		
+	}
+	
+	
+}
---
+++ SistemasCoordenadas2D/src/sistemascoordenadas2d/Esferica.java
@@ -0,0 +1,5 @@
+package sistemascoordenadas2d;
+
+public class Esferica {
+
+}
